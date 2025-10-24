ab5c4fd7242bd783eeb010a9cda16cfb78dc6736
YigithanGumus <yigithangumuss@gmail.com>
2025-10-24 21:41:27 +0300

    Update AuthController.php
    Image upload fields have been added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,32 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
+use App\Traits\ManageFiles;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
+    use ManageFiles;
+
     public function register(UserRegisterRequest $request)
     {
+        $profileImagePath = null;
+        $channelImagePath = null;
+        $bannerPath = null;
+
+        if ($request->hasFile('profile_image')) {
+            $profileImagePath = $this->uploadFile(
+                $request->file('profile_image'),
+                'uploads/profile_images'
+            );
+        }
+
+        if ($request->hasFile('channel_image')) {
+            $channelImagePath = $this->uploadFile(
+                $request->file('channel_image'),
+                'uploads/channel_images'
+            );
+        }
+
+        if ($request->hasFile('banner')) {
+            $bannerPath = $this->uploadFile(
+                $request->file('banner'),
+                'uploads/banners'
+            );
+        }
+
         $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
-            'password' => bcrypt($request->password),
+            'name'          => $request->name,
+            'email'         => $request->email,
+            'password'      => bcrypt($request->password),
+            'profile_image' => $profileImagePath,
         ]);
 
         $user->channel()->create([
-            'name' => $request->channel_name,
-            'slug' => Str::slug($request->channel_name, '-'),
-            'uid' => uniqid(true),
+            'name'          => $request->channel_name,
+            'slug'          => Str::slug($request->channel_name, '-'),
+            'uid'           => uniqid(true),
+            'image' => $channelImagePath,
+            'banner'        => $bannerPath,
         ]);
 
         auth()->login($user);
 
         return response()->json([
-            'success' => true,
-            'message' => 'Kayıt başarılı! Hoş geldiniz.',
-            'redirect' => route('login.page'),
+            'success'  => true,
+            'message'  => 'Kayıt başarılı! Hoş geldiniz.',
+            'redirect' => route('home'),
         ]);
     }
 
